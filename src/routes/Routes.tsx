1a666ef904ec945dd67008f4aa362d77caf9d04d
Md Soyaib Hossain <mdsoyaibhossain@macmini24.local>
2025-07-15 16:29:58 +0600

    delete some file

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,15 +1,9 @@
+import App from "@/App";
+import HomePage from "@/pages/Home";
+import NotFound from "@/pages/NotFound";
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
-import NotFound from "../pages/NotFound";
-import Home from "../pages/Home";
-import AdminRoute from "./AdminRoutes";
-import AdminDashboard from "@/pages/Admin/AdminDashboard";
-import Login from "@/pages/Login";
-import Signup from "@/pages/Signup";
-import Form from "@/pages/Form";
-import Services from "@/pages/Services";
+
+
 
 const routes = createBrowserRouter([
   {
@@ -18,38 +12,7 @@ const routes = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/services",
-        element: <Services />,
-      },
-      {
-        path: "/form",
-        element: <Form />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/signup",
-        element: <Signup />,
-      },
-      {
-        path: "/admin",
-        element: <AdminRoute />, // This will check if the user is an admin
-        children: [
-          { path: "", element: <AdminDashboard /> }, // Admin Dashboard
-        ],
+        element: <HomePage />,
       },
     ],
   },

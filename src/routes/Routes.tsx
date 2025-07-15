6149059bd78210606dd1b23c019372c2bf8c4e15
Md Soyaib Hossain <mdsoyaibhossain@macmini24.local>
2025-07-15 16:23:48 +0600

    first commit

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,62 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import NotFound from "../pages/NotFound";
+import Home from "../pages/Home";
+import AdminRoute from "./AdminRoutes";
+import AdminDashboard from "@/pages/Admin/AdminDashboard";
+import Login from "@/pages/Login";
+import Signup from "@/pages/Signup";
+import Form from "@/pages/Form";
+import Services from "@/pages/Services";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
+      },
+      {
+        path: "/form",
+        element: <Form />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/admin",
+        element: <AdminRoute />, // This will check if the user is an admin
+        children: [
+          { path: "", element: <AdminDashboard /> }, // Admin Dashboard
+        ],
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default routes;

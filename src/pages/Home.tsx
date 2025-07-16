1e38f45f063c7c98961bd3a7b68c38453b0550e0
Md Soyaib Hossain <mdsoyaibhossain@macmini24.local>
2025-07-16 11:13:57 +0600

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import CommonWrapper from "@/common/CommonWrapper";
 import Banner from "@/components/home/Banner";
-import Category from "@/components/home/Category";
 
 const Home = () => {
   return (
     <CommonWrapper>
       <Banner></Banner>
-      <Category></Category>
     </CommonWrapper>
   );
 };

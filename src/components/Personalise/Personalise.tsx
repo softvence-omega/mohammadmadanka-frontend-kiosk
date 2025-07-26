1a6e1c4bae91ef8236152737f98552015a444196
mdtamim1001 <mdtamim26301@gmail.com>
2025-07-26 14:37:29 +0600

    sat

--- src/components/Personalise/Personalise.tsx
+++ src/components/Personalise/Personalise.tsx
@@ -8,7 +8,7 @@ export default function PersonaliseButton({ onClick }: PersonaliseButtonProps) {
       onClick={onClick}
       className="flex flex-row justify-center items-center px-6 py-4 gap-2 w-full max-w-[379px] h-14 bg-[#FF5757] rounded-[49px] hover:bg-[#e54545] transition-colors"
     >
-      <span className="text-white text-xl font-normal leading-[120%]">Personalise</span>
+      <span className="text-white text-xl font-[Baloo] font-normal eading-[120%]">Personalise</span>
     </button>
   );
 }
--- src/components/addToCard/AddButton.tsx
+++ src/components/addToCard/AddButton.tsx
@@ -12,7 +12,7 @@ export default function AddButton({ onAddToCart }: AddButtonProps) {
         onClick={onAddToCart}
         className="flex flex-row mt-10 justify-center items-center px-6 py-4 gap-2 w-full max-w-[379px] h-16 bg-[#FF5757] rounded-[49px] hover:bg-[#e54545] transition-colors"
       >
-        <span className="text-white text-xl font-normal leading-[120%]">Add To Cart</span>
+        <span className="text-white text-xl font-baloo font-normal ">Add To Cart</span>
         <ShoppingCart className="w-8 h-8 text-white" />
       </button>
     </div>
